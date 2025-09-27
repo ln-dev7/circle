170b1e785c0d83e48470524ffc08bfcfd759a018
ln-dev7 <leonelngoya@gmail.com>
2025-09-27 23:19:46 +0100

    fix: remove unused ListFilter import in header options

--- components/layout/headers/teams/header-options.tsx
+++ components/layout/headers/teams/header-options.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { ListFilter, SlidersHorizontal } from 'lucide-react';
+import { SlidersHorizontal } from 'lucide-react';
 import { Filter } from './filter';
 
 export default function HeaderOptions() {
