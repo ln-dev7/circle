c60371cc882efc21fe1d0941e09c3fc19d5ced10
ln-dev7 <leonelngoya@gmail.com>
2026-08-04 16:38:21 +0000

    add the cmd+k command palette (issue actions, copy commands, navigation)

--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppSidebar } from '@/components/layout/sidebar/app-sidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { CreateIssueModalProvider } from '@/components/common/issues/create-issue-modal-provider';
+import { CommandPalette } from '@/components/layout/command-palette';
 import { cn } from '@/lib/utils';
 
 interface MainLayoutProps {
@@ -34,6 +35,7 @@ export default function MainLayout({ children, header, headersNumber = 2 }: Main
    return (
       <SidebarProvider>
          <CreateIssueModalProvider />
+         <CommandPalette />
          <AppSidebar />
          <div className="h-svh overflow-hidden lg:p-2 w-full">
             <div className="lg:border lg:rounded-md overflow-hidden flex flex-col items-center justify-start bg-container h-full w-full">
